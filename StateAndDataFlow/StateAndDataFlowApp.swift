c112d75e762ff628f986fe839c3c2c78808e7456
aizaomar <w.omaraiza@gmail.com>
2021-11-10 00:06:38 +0600

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Aiza on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
